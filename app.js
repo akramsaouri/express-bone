520aa8bfcf679db23f8ccb6ac2c23ced7fa6cfe9
akramsaouri <akram_saouri@hotmail.fr>
2016-06-05 16:20:59 +0000

    added .env fie template

--- app.js
+++ app.js
@@ -17,16 +17,28 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'SecretKey', resave: true, saveUninitialized: true, cookie: {}}));
+app.use(session({
+    secret: 'SecretKey',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {}
+}));
 app.use(flash());
 app.use(middleware.auth);
 app.use('/', router);
 
+/*
+require('dotenv').config()
+console.log(process.env.ENV_TEST_NAME);
+*/
+
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -37,7 +49,7 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
+    app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -48,7 +60,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,

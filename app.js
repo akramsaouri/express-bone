92644ce423088c14e918ce02cce502f072d1a051
Akram Saouri <akram_saouri@hotmail.fr>
2016-06-13 00:21:00 +0000

    config file

--- app.js
+++ app.js
@@ -1,72 +1,41 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var flash = require('connect-flash');
-
-var router = require('./router/router');
-var middleware = require('./middlewares/middleware');
 var app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.set('port', process.env.PORT || 3000)
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-    secret: 'SecretKey',
-    resave: true,
-    saveUninitialized: true,
-    cookie: {}
-}));
-app.use(flash());
-app.use(middleware.auth);
-app.use('/', router);
+require('dotenv').config();
+require('./config')(app,express);
 
-/*
-require('dotenv').config()
-console.log(process.env.ENV_TEST_NAME);
-*/
+app.use('/', require('./router/router'));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
+//
+// // error handlers
+//
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+//     app.use(function(err, req, res, next) {
+//         res.status(err.status || 500);
+//         res.render('error', {
+//             message: err.message,
+//             error: err
+//         });
+//     });
+// }
+//
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//         message: err.message,
+//         error: {}
+//     });
+// });
 
 
 app.listen(app.get('port'), function() {
---
+++ config/index.js
@@ -0,0 +1,27 @@
+module.exports = function(app, express) {
+
+    var path = require('path');
+    var bodyParser = require('body-parser');
+
+    // Global Configs
+    app.set('views', path.join(__dirname, '../views'));
+    app.set('view engine', 'ejs');
+    app.set('port', process.env.PORT || 3000);
+
+
+    // Middlewares
+    // [Optional] app.use(require('serve-favicon')(path.join(__dirname, '../public', 'favicon.ico')));
+    app.use(require('morgan')('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
+    app.use(require('cookie-parser')());
+    app.use(express.static(path.join(__dirname, '../public')));
+    app.use(require('express-session')({
+        secret: 'SecretKat',
+        resave: true,
+        saveUninitialized: true
+    }));
+    app.use(require('connect-flash')());
+}

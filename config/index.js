58c7dfac240c0685b61e6f655deb1bbf73b452b2
Akram Saouri <akram_saouri@hotmail.fr>
2016-06-13 02:12:46 +0000

    added few comments

--- config/index.js
+++ config/index.js
@@ -6,26 +6,26 @@ module.exports = function(app) {
     var bodyParser = require('body-parser');
 
     // Global Configs
-    app.set('views', path.join(__dirname, '../views'));
-    app.set('view engine', 'ejs');
-    app.set('port', process.env.PORT || 3000);
+    app.set('views', path.join(__dirname, '../views')); // views engine
+    app.set('view engine', 'ejs'); // views engine template
+    app.set('port', process.env.PORT || 3000); // port
 
 
     // Middlewares
     // [Optional] app.use(require('serve-favicon')(path.join(__dirname, '../public', 'favicon.ico')));
-    app.use(require('morgan')('dev'));
-    app.use(bodyParser.json());
+    app.use(require('morgan')('dev')); // morgan logging
+    app.use(bodyParser.json()); // body parser
     app.use(bodyParser.urlencoded({
         extended: false
     }));
-    app.use(require('cookie-parser')());
-    app.use(express.static(path.join(__dirname, '../public')));
-    app.use(require('express-session')({
+    app.use(require('cookie-parser')()); //cookie parser
+    app.use(express.static(path.join(__dirname, '../public'))); // static files
+    app.use(require('express-session')({ // http session
         secret: 'SecretKat',
         resave: true,
         saveUninitialized: true
     }));
-    app.use(require('connect-flash')());
+    app.use(require('connect-flash')()); // flash session
 
     app.set('env', 'dev'); // env : dev || prod
 }
